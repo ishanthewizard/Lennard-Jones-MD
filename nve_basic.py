7180cf88c0001445671cd4c22c976269b4e35426
ishanthewizard <77704916+ishanthewizard@users.noreply.github.com>
2023-05-31 17:03:05 -0700

    finished initializing velocities

--- nve_basic.py
+++ nve_basic.py
@@ -2,9 +2,10 @@ import numpy as np
 import gsd.hoomd
 import math
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 # Initial parameters
-n_particle = 256  # number of particle
+n_particle = 100  # number of particle
 temp = 1.0  # temperature in reduced units
 box = 10
 epsilon = 1  # LJ epsilon
@@ -73,6 +74,8 @@ radii = fcc_positions(n_particle, box)
 radii = radii - 0.5  # convert to -0.5 to 0.5 space for ease with PBC
 
 # Procedure to initialize velocities
+velocities = np.random.normal(0, np.sqrt(temp), (n_particle, 3))
+velocities -= np.mean(velocities, axis=0)
 
 
 # You can get energy and the pressure for free out of this calculation if you do it right
@@ -86,8 +89,6 @@ def force_calc(box, radii, sigma, epsilon):
 
     # # minumum image convention
     dr = dr - box * np.rint(dr / box)
-    # dr = np.where(dr < -0.5, dr - 1, dr) ????? why doesn't this work
-    # dr = np.where(dr > 0.5, dr + 1, dr) ????  why doesn't this work
 
     # Calculate squared distances
     dr_sq = np.sum(dr**2, axis=-1)
@@ -99,18 +100,21 @@ def force_calc(box, radii, sigma, epsilon):
     # Calculate the magnitudes of forces using the Lennard-Jones potential
     inv_dr6 = (sigma**2 / dr_sq) ** 3
     f_mag = (48 * epsilon / dr_sq) * (inv_dr6**2 - 0.5 * inv_dr6)
+
     # Set the diagonal back to zero
     f_mag[mask] = 0.0
 
     forces = np.sum(f_mag[:, :, np.newaxis] * dr, axis=1)
 
-    if np.max(np.abs(0.5 * dt * forces)) > 10:
-        print(glob_step)
-        print(np.max(np.abs(0.5 * dt * f_mag)))
-        print("min radius", np.min(np.abs(dr_sq)))
-        assert 1 == 0
+    # Calculate the potential energy
+    potential_energy = np.sum(4 * epsilon * (inv_dr6**2 - inv_dr6))
+    # We set the diagonal of the potential energy to zero as we do not want to include self-interactions
+    potential_energy -= n_particle * 4 * epsilon * (sigma**12 - sigma**6)
+    # Calculate the kinetic energy
+    kinetic_energy = 0.5 * np.sum(np.linalg.norm(velocities, axis=1) ** 2)
+    total_energy = potential_energy + kinetic_energy
 
-    return forces
+    return forces, total_energy
 
 
 # Function to dump simulation frame that is readable in Ovito
@@ -155,6 +159,7 @@ print("%d" % (0), file=f)
 # NVE integration
 # Equilibration
 glob_step = 0
+total_energies = []
 for step in tqdm(range(nsteps)):
     glob_step = step
     # Velocity Verlet algorithm
@@ -164,10 +169,11 @@ for step in tqdm(range(nsteps)):
     # Applying PBC
     radii = np.where(np.abs(radii) > 0.5, (radii + 0.5) % 1 - 0.5, radii)
 
-    forces = force_calc(box, radii, sigma, epsilon)
+    forces, total_energy = force_calc(box, radii, sigma, epsilon)
     velocities = velocities + 0.5 * dt * forces
 
     # dump frame
+    total_energies.append(total_energy)
     if step % (n_dump // 2) == 0:
         t.append(create_frame(radii, velocities, sigma, box, step / n_dump))
 
@@ -176,4 +182,26 @@ for step in tqdm(range(nsteps)):
 # way to compute g_r
 # I recommend analyzing diffusion coefficient from the trajectories you dump
 
+
+def plot_energy(total_energies):
+    # Create an array or list for the step numbers
+    steps = range(len(total_energies))
+
+    # Create the plot
+    plt.figure(figsize=(10, 6))
+    plt.plot(steps, total_energies, label="Total Energy")
+
+    # Add labels and title
+    plt.xlabel("Steps")
+    plt.ylabel("Total Energy")
+    plt.title("Total Energy per Step")
+
+    # Add a legend
+    plt.legend()
+
+    # Show the plot
+    plt.show()
+
+
+plot_energy(total_energies)
 f.close()
